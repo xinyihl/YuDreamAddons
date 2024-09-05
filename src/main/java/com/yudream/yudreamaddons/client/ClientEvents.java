e4f3651784077f562bd84fe6ec7887ea31802726
xinyihl <1012737146@qq.com>
2024-09-05 16:31:38 +0800

    通过快捷键 `L` 获取物品 id 列表

--- src/main/java/com/yudream/yudreamaddons/client/ClientEvents.java
+++ src/main/java/com/yudream/yudreamaddons/client/ClientEvents.java
@@ -7,72 +7,93 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextComponentString;
-import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.client.event.GuiScreenEvent.KeyboardInputEvent.Pre;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.input.Keyboard;
 
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
+import java.awt.*;
 import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.Transferable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class ClientEvents {
-    public static final KeyBinding KEY_PLACE = new KeyBinding("key.yudreamaddons.getitemId", Keyboard.KEY_K, "key.yudreamaddons.desc");
+    public static final KeyBinding KEY_GET_ITEM_ID = new KeyBinding("key.yudreamaddons.getItemId", Keyboard.KEY_K, "key.yudreamaddons.desc");
+    public static final KeyBinding KEY_GET_ITEM_ID_LIST = new KeyBinding("key.yudreamaddons.getItemIdList", Keyboard.KEY_L, "key.yudreamaddons.desc");
+    private static final List<String> itemList = new ArrayList<>();
 
     public ClientEvents() {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     public static void setSysClipboardText(String writeMe) {
-        Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
-        Transferable tText = new StringSelection(writeMe);
-        clip.setContents(tText, null);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(writeMe), null);
     }
 
     public void registerKeyBindings() {
-        ClientRegistry.registerKeyBinding(ClientEvents.KEY_PLACE);
+        ClientRegistry.registerKeyBinding(ClientEvents.KEY_GET_ITEM_ID);
+        ClientRegistry.registerKeyBinding(ClientEvents.KEY_GET_ITEM_ID_LIST);
     }
 
     @SubscribeEvent
-    public void onGuiKeyboardEvent(GuiScreenEvent.KeyboardInputEvent.Pre event) {
+    public void onGuiKeyboardEvent(Pre event) {
         if (handleKeyEvent(event)) {
             event.setCanceled(true);
         }
     }
 
-    private boolean handleKeyEvent(GuiScreenEvent.KeyboardInputEvent.Pre event) {
+    private boolean handleKeyEvent(Pre event) {
         char typedChar = Keyboard.getEventCharacter();
         int eventKey = Keyboard.getEventKey();
-
-        return ((eventKey == 0 && typedChar >= 32) || Keyboard.getEventKeyState()) &&
-                handleKeyDown(eventKey, event);
+        return ((eventKey == 0 && typedChar >= 32) || Keyboard.getEventKeyState()) && handleKeyDown(eventKey, event);
     }
 
-    private boolean handleKeyDown(int eventKey, GuiScreenEvent.KeyboardInputEvent.Pre event) {
+    private boolean handleKeyDown(int eventKey, Pre event) {
         Minecraft mc = Minecraft.getMinecraft();
         EntityPlayerSP player = mc.player;
         GuiScreen gui = event.getGui();
-        if (eventKey == KEY_PLACE.getKeyCode()) {
+        if (eventKey == KEY_GET_ITEM_ID.getKeyCode()) {
             if (player != null && gui instanceof GuiContainer) {
                 GuiContainer guiContainer = (GuiContainer) gui;
                 ItemStack hoveredStack = guiContainer.getSlotUnderMouse() != null ? guiContainer.getSlotUnderMouse().getStack() : ItemStack.EMPTY;
                 if (!hoveredStack.isEmpty()) {
-                    int meta = hoveredStack.getMetadata();
-                    String text = "<" + Objects.requireNonNull(hoveredStack.getItem().getRegistryName()) + (meta == 0 ? "" : ":" + meta) + ">";
-                    if (hoveredStack.serializeNBT().hasKey("tag")) {
-                        String nbt = hoveredStack.serializeNBT().getTag("tag").toString();
-                        if (nbt.length() > 0)
-                            text += ".withTag(" + nbt + ")";
-                    }
-                    player.sendMessage(new TextComponentString("Item: " + text));
-                    setSysClipboardText(text);
+                    String item = getItemId(hoveredStack);
+                    player.sendMessage(new TextComponentString("Item: " + item));
+                    setSysClipboardText(item);
                 }
+                return true;
                 //player.sendMessage(new TextComponentString("Current GUI: " + guiContainer.getClass().getSimpleName()));
             }
         }
+        if (eventKey == KEY_GET_ITEM_ID_LIST.getKeyCode()) {
+            if (player != null && gui instanceof GuiContainer) {
+                GuiContainer guiContainer = (GuiContainer) gui;
+                ItemStack hoveredStack = guiContainer.getSlotUnderMouse() != null ? guiContainer.getSlotUnderMouse().getStack() : ItemStack.EMPTY;
+                if (!hoveredStack.isEmpty()) {
+                    String item = getItemId(hoveredStack);
+                    itemList.add(item);
+                    player.sendMessage(new TextComponentString("ItemList Add: " + item));
+                } else {
+                    player.sendMessage(new TextComponentString("ItemList: " + itemList));
+                    setSysClipboardText(itemList.toString());
+                    itemList.clear();
+                }
+                return true;
+            }
+        }
         return false;
+    }
+
+    private String getItemId(ItemStack itemStack) {
+        int meta = itemStack.getMetadata();
+        String text = "<" + Objects.requireNonNull(itemStack.getItem().getRegistryName()) + (meta == 0 ? "" : ":" + meta) + ">";
+        if (itemStack.serializeNBT().hasKey("tag")) {
+            String nbt = itemStack.serializeNBT().getTag("tag").toString();
+            if (nbt.length() > 0)
+                text += ".withTag(" + nbt + ")";
+        }
+        return text;
     }
 }
