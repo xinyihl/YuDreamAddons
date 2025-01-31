788a572d309eb5364aa14935ee23f39ff0647117
xinyihl <1012737146@qq.com>
2025-01-31 11:38:26 +0800

    修改获取物品 ID 实现方式支持 jei

--- src/main/java/com/yudream/yudreamaddons/common/client/ClientEventHandler.java
+++ src/main/java/com/yudream/yudreamaddons/common/client/ClientEventHandler.java
@@ -2,12 +2,12 @@ package com.yudream.yudreamaddons.common.client;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextComponentString;
-import net.minecraftforge.client.event.GuiScreenEvent.KeyboardInputEvent.Pre;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.input.Keyboard;
@@ -19,13 +19,19 @@ import java.util.List;
 import java.util.Objects;
 
 public class ClientEventHandler {
+
+    private static boolean keyDown1 = false;
+    private static boolean keyDown2 = false;
+
     public static final KeyBinding KEY_GET_ITEM_ID = new KeyBinding("key.yudreamaddons.getItemId", Keyboard.KEY_K, "key.yudreamaddons.desc");
     public static final KeyBinding KEY_GET_ITEM_ID_LIST = new KeyBinding("key.yudreamaddons.getItemIdList", Keyboard.KEY_L, "key.yudreamaddons.desc");
+    public static final KeyBinding KEY_GET_ITEM_ID_LIST_OUT = new KeyBinding("key.yudreamaddons.getItemIdListOut", Keyboard.KEY_J, "key.yudreamaddons.desc");
     private static final List<String> itemList = new ArrayList<>();
 
     public ClientEventHandler() {
         ClientRegistry.registerKeyBinding(KEY_GET_ITEM_ID);
         ClientRegistry.registerKeyBinding(KEY_GET_ITEM_ID_LIST);
+        ClientRegistry.registerKeyBinding(KEY_GET_ITEM_ID_LIST_OUT);
     }
 
     public static void setSysClipboardText(String writeMe) {
@@ -33,19 +39,75 @@ public class ClientEventHandler {
     }
 
     @SubscribeEvent
-    public void onGuiKeyboardEvent(Pre event) {
+    public void onRenderTooltipEvent(ItemTooltipEvent event) {
+        ItemStack itemStack = event.getItemStack();
+        EntityPlayer player = event.getEntityPlayer();
+
+        if (player == null) return;
+
+        if (keyDown1) {
+            keyDown1 = false;
+            String item = getItemId(itemStack);
+            player.sendMessage(new TextComponentString("Item: " + item));
+            setSysClipboardText(item);
+        }
+
+        if (keyDown2) {
+            keyDown2 = false;
+            String item = getItemId(itemStack);
+            itemList.add(item);
+            player.sendMessage(new TextComponentString("ItemList Add: " + item));
+        }
+    }
+
+    @SubscribeEvent
+    public void onKeyPressed(GuiScreenEvent.KeyboardInputEvent.Post event) {
+        //char typedChar = Keyboard.getEventCharacter();
+        int keyCode = Keyboard.getEventKey();
+        boolean isPressed = Keyboard.getEventKeyState();
+        if(keyCode != 0 /*&& typedChar >= 32*/){
+            if(isPressed){ // 按下
+                if (keyCode == KEY_GET_ITEM_ID.getKeyCode()) {
+                    keyDown1 = true;
+                }
+                if (keyCode == KEY_GET_ITEM_ID_LIST.getKeyCode()) {
+                    keyDown2 = true;
+                }
+                if (keyCode == KEY_GET_ITEM_ID_LIST_OUT.getKeyCode()) {
+                    EntityPlayerSP player = Minecraft.getMinecraft().player;
+                    if (player != null) {
+                        player.sendMessage(new TextComponentString("ItemList: " + itemList));
+                        setSysClipboardText(itemList.toString());
+                        itemList.clear();
+                    }
+                }
+            }
+            if(!isPressed){ // 抬起
+                if (keyCode == KEY_GET_ITEM_ID.getKeyCode()) {
+                    keyDown1 = false;
+                }
+                if (keyCode == KEY_GET_ITEM_ID_LIST.getKeyCode()) {
+                    keyDown2 = false;
+                }
+            }
+        }
+    }
+
+/*
+    @SubscribeEvent
+    public void onGuiKeyboardEvent(GuiScreenEvent.KeyboardInputEvent.Post event) {
         if (handleKeyEvent(event)) {
             event.setCanceled(true);
         }
     }
 
-    private boolean handleKeyEvent(Pre event) {
+    private boolean handleKeyEvent(GuiScreenEvent.KeyboardInputEvent.Post event) {
         char typedChar = Keyboard.getEventCharacter();
         int eventKey = Keyboard.getEventKey();
         return eventKey != 0 && typedChar >= 32 && Keyboard.getEventKeyState() && handleKeyDown(event, eventKey);
     }
 
-    private boolean handleKeyDown(Pre event, int eventKey) {
+    private boolean handleKeyDown(GuiScreenEvent.KeyboardInputEvent.Post event, int eventKey) {
         Minecraft mc = Minecraft.getMinecraft();
         EntityPlayerSP player = mc.player;
         GuiScreen gui = event.getGui();
@@ -79,6 +141,7 @@ public class ClientEventHandler {
         }
         return false;
     }
+*/
 
     private String getItemId(ItemStack itemStack) {
         int meta = itemStack.getMetadata();
